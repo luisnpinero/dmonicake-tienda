b7614a4478494ed66642892f291ff00c9ce5e2e1
Luis Enrique <luisnpinero@gmail.com>
2020-12-20 13:16:38 -0500

    agregando el autocompletar al formulario de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,12 +14,12 @@
 
             <div>
                 <x-jet-label for="first_name" value="{{ __('Nombres') }}" />
-                <x-jet-input id="first_name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="last_name" value="{{ __('Apellidos') }}" />
-                <x-jet-input id="last_name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="last_name" />
             </div>
 
             <div class="mt-4">
@@ -29,17 +29,22 @@
 
             <div class="mt-4">
                 <x-jet-label for="country_code" value="{{ __('Código de País') }}" />
-                <x-jet-input id="country_code" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input id="country_code" class="block mt-1 w-full" type="text" name="country_code" :value="old('country_code')" required />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="phone_number" value="{{ __('Número de Teléfono') }}" />
-                <x-jet-input id="phone_number" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input id="phone_number" class="block mt-1 w-full" type="phone" name="phone_number" :value="old('phone_number')" required />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="address" value="{{ __('Dirección') }}" />
-                <x-jet-input id="address" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="address" value="{{ __('gugle') }}" />
+                <x-jet-input type="text" name="autocomplete" id="autocomplete" class="form-control" placeholder="Select Location" required />
             </div>
 
             <div class="mt-4">
@@ -61,6 +66,30 @@
                     {{ __('Finalizar Registro') }}
                 </x-jet-button>
             </div>
+
+            {{-- javascript code --}}
+            <script src="https://maps.google.com/maps/api/js?key=AIzaSyDxTV3a6oL6vAaRookXxpiJhynuUpSccjY&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
+                <script>
+                    $(document).ready(function() {
+                    $("#lat_area").addClass("d-none");
+                    $("#long_area").addClass("d-none");
+                    });
+                </script>
+                <script>
+                    google.maps.event.addDomListener(window, 'load', initialize);
+                    function initialize() {
+                    var input = document.getElementById('autocomplete');
+                    var autocomplete = new google.maps.places.Autocomplete(input);
+                    autocomplete.addListener('place_changed', function() {
+                    var place = autocomplete.getPlace();
+                    $('#latitude').val(place.geometry['location'].lat());
+                    $('#longitude').val(place.geometry['location'].lng());
+                    // --------- show lat and long ---------------
+                    $("#lat_area").removeClass("d-none");
+                    $("#long_area").removeClass("d-none");
+                });
+            }
+            </script>
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('auto-complete', [GoogleController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
