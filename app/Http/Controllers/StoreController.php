27633787863f08e6e81ebc64dc26831bc62631e9
Luis Enrique <luisnpinero@gmail.com>
2021-01-27 19:17:08 -0500

    haciendo merge

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -10,12 +10,7 @@ class StoreController extends Controller
 {
     public function index(){
         $categories = Category::all();
-<<<<<<< HEAD
-        $products = Product::all();
-        
-=======
         $products = Product::paginate(12);
->>>>>>> 8417a9faa698e32ca418fa48a62625f8f09cce23
         return view('store.index')->with([
             'categories' => $categories,
             'products' => $products
@@ -27,14 +22,6 @@ class StoreController extends Controller
         // return view('index');
     }
 
-<<<<<<< HEAD
-    public function product_show($product){
-        $product = Product::where('name', $product)->first();
-        return view('store.show')->with([
-            'product' => $product,
-        ]);
-        // return view('index');
-=======
     public function show_product($product){
         //$category = Category::where('name',$category)->first();
         $product = Product::where('name',$product)->first();
@@ -42,7 +29,6 @@ class StoreController extends Controller
         return view('store.show')->with([
             'product' => $product,
         ]);
->>>>>>> 8417a9faa698e32ca418fa48a62625f8f09cce23
     }
 
     public function categories($category){
